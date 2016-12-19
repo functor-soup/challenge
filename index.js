59a764f123d338f533cd0d00f0fd285d8a1efde6
luke <nerorevenge@gmail.com>
2016-12-18 22:55:07 -0500

    Effects of adding new date realted validation  - correction for incorrect use of currying

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const utils = require("./lib/utils");
 const config = require("./config/config");
 const M = require("monet");
 const R = require("ramda");
+const moment = require("moment");
 const parseString = require('xml2json-light').xml2json;
 const args = require("yargs")
     .option('p', {
@@ -38,20 +39,37 @@ try {
     process.exit(1);
 };
 
-const validate = R.curry(utils.validateJson)(R.__)(config.maxSpaces);
+const appStartTime = moment();
+
+const validate = R.curry(utils.validateJson)(R.__,config.maxSpaces);
+const validateDates = R.curry(utils.validateJsonDates)(R.__,appStartTime);
 
 const state = utils.validateXML(fileStrings[0], fileStrings[1])
     .bind(function(xmlString) {
         const lensPath = R.lensPath(['cars', 'car'])
-        const lens = R.lensPath(['id']);
-
         const data = parseString(xmlString);
         const view = R.view(lensPath, data);
-        return utils.validateAttributeValue(view, lens, validate, "error msg");
+        // tranform datetime strings into moment objects
+        return M.Either.Right(utils.transformDateTime(view, R.lensProp('parkingtime')));
+    })
+    .bind(function(view) {
+        const lens = R.lensProp('parkinglotid');
+        return utils.validateAttributeValue(view, lens, validate,
+            "exceeds total number of spaces in on parking lot");
+    })
+    .bind(function(view) {
+        const lens = R.lensProp('parkingtime');
+        return utils.validateAttributeValue(view, lens, validateDates,
+            "One or more of Dates is before app start time");
     })
     .cata(function(x) {
         console.error(x)
         process.exit(1)
     }, function(x) {
-        return x;
+	// group by parking lot id
+        return R.groupBy(function(x) {
+            return x.parkinglotid;
+        }, x);
     });
+
+console.log(state);
